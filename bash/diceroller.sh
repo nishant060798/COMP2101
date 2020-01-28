ddc852ea6fbbdc7469dedd269915fd4a1f10cac0
Nishant Sudhir <nishant060798@gmail.com>
2020-01-28 15:22:52 -0500

    lab2

---
+++ bash/diceroller.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# this script rolls a six-sided die
+#
+
+# Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
+
+# roll the dice and display the result
+
+echo "
+Rolling a pair of six sided dice...
+$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)) $(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1)),$(( RANDOM % 6 + 1))rolled
+"
--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -21,5 +21,5 @@
 
 echo "Setuid files:"
 echo "============="
-find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
+find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
 echo ""
